bc8d1e9cd3564fdc796099cefad8b5f021463268
Brendon <brennon.mckeever@gmail.com>
2017-04-18 16:36:54 -0500

    continue work on daily tasks

--- LMNOPproject/fetch_ticketmaster_each_day.py
+++ LMNOPproject/fetch_ticketmaster_each_day.py
@@ -9,6 +9,7 @@ import psycopg2
 
 try:
 
+
     db = psycopg2.connect(database='lmnop', user='lmnop', password='yankees7')
     cur = db.cursor()
 
@@ -17,11 +18,15 @@ try:
     # rows = cur.fetchall()
     # print(rows)
 
-    artist = ('Norah Jones',)
-    #search = 'SELECT * FROM lmn_artist WHERE name = %s'
-    cur.execute('SELECT * FROM lmn_artist WHERE name=?', ('Norah Jones')) # TODO can't seem to figure out how to pass a string into query.
-    rows = cur.fetchall()
-    print(rows)
+    # artist = 'Brendon McKeever'
+    # cur.execute('SELECT * FROM lmn_artist WHERE name=%s', (artist,)) # TODO can't seem to figure out how to pass a string into query.
+    # rows = cur.fetchall()
+    # print(rows)
+    # print(type(rows))
+
+    # query = 'INSERT INTO lmn_artist (name) VALUES (%s)'
+    # cur.execute(query, (artist,))
+    # db.commit()
 
 
     # start the daily task of adding events to database from ticketmaster.
@@ -31,10 +36,13 @@ try:
 
     # getting time and formatting it for ticketmaster.
     time = datetime.utcnow()
-    start_time = time + timedelta(days=1) - timedelta(hours=13)
+    start_time = time + timedelta(days=1) - timedelta(hours=5)
     final_start = start_time.strftime("%Y-%m-%dT%H:%M:%SZ")
-    end_time = time + timedelta(days=2) - timedelta(hours=13)
+    end_time = time + timedelta(days=2) - timedelta(hours=5)
     final_end = end_time.strftime("%Y-%m-%dT%H:%M:%SZ")
+    print(final_start)
+    print(final_end)
+
 
     url = base_url.format(key,final_start,final_end)
 
@@ -46,54 +54,88 @@ try:
 
 
 
-    # show_list = dict()
-    #
-    # try:
-    #
-    #     artist = tm_json["_embedded"]['events']
-    #
-    #     # Loop over json and pull relevant info.
-    #     for entry in artist:
-    #         for place in entry["_embedded"]['venues']:
-    #             for artists in entry["_embedded"]['attractions']:
-    #
-    #                 artist = artists['name']
-    #                 location = place['name']
-    #                 day = entry["dates"]["start"]["localDate"]
-    #                 time = entry["dates"]["start"]["localTime"]
-    #
-    #                 date_time = day + " " + time
-    #
-    #                 venue_list = []
-    #
-    #                 venue_list.append(location)
-    #                 venue_list.append(date_time)
-    #
-    #                 show_list[artist] = venue_list
-    #
-    #
-    #     print(show_list)
-    #
-    #     value_list = []
-    #
-    #     # loop over created dictionary and add show/artist to database.
-    #     for key, value in show_list.items():
-    #
-    #         name = key
-    #         value_list = show_list[key]
-    #         location = value_list[0]
-    #         date = value_list[1]
-    #
-    #
-    #         artist_query = Artist.objects.filter(name = name)
-    #         venue_query = Venue.objects.filter(name = location)
-    #         print(artist_query)
-    #
-    #         # two checks to see if artist or venue don;t exist in database
-    #         if not artist_query:
-    #
-    #             #print("no artist found")
-    #             artist = Artist.objects.create(name = name)
+    show_list = dict()
+
+    try:
+
+        artist = tm_json["_embedded"]['events']
+
+
+        # Loop over json and pull relevant info.
+        for entry in artist:
+            for place in entry["_embedded"]['venues']:
+                for performer in entry["_embedded"]['attractions']:
+
+                    artist = ''
+
+                    #oddly I found that not every show has a attraction name.
+                    try:
+                        artist = performer['name']
+
+                    except Exception as e:
+                        artist = entry['name']
+
+                    location = place['name']
+
+                    cur.execute('SELECT * FROM lmn_venue WHERE name=%s', (location,))
+                    venue_rows = cur.fetchall()
+
+                    if not venue_rows:
+
+                        city = place['city']['name']
+                        print(location)
+                        print(city)
+
+                    day = entry["dates"]["start"]["localDate"]
+                    time = entry["dates"]["start"]["localTime"]
+
+                    date_time = day + " " + time
+
+                    venue_list = []
+
+                    venue_list.append(location)
+                    venue_list.append(date_time)
+
+                    show_list[artist] = venue_list
+
+
+        #print(show_list)
+
+        value_list = []
+
+        # loop over created dictionary and add show/artist to database.
+        for key, value in show_list.items():
+
+            name = key
+            value_list = show_list[key]
+            location = value_list[0]
+            date = value_list[1]
+
+            cur.execute('SELECT * FROM lmn_artist WHERE name=%s', (name,))
+            artist_rows = cur.fetchall()
+
+            cur.execute('SELECT * FROM lmn_venue WHERE name=%s', (location,))
+            venue_rows = cur.fetchall()
+
+            artist_id = 0
+
+            #check if artist is in database.
+            if not artist_rows:
+
+                query = 'INSERT INTO lmn_artist (name) VALUES (%s)'
+                cur.execute(query, (name,))
+                db.commit()
+
+            #need to get artist ID to enter show information.
+            cur.execute('SELECT * FROM lmn_artist WHERE name=%s', (name,))
+            artist_rows = cur.fetchall()
+            artist_id = artist_rows[0][0]
+
+
+
+            # cur.execute('SELECT * FROM lmn_show WHERE name=%s AND ', (location,))
+            # event_rows = cur.fetchall()
+
     #
     #         artist_query = Artist.objects.filter(name = name)
     #
@@ -106,9 +148,9 @@ try:
     #
     #
     #
-    # except Exception as e:
-    #
-    #     logging.exception("Problem!")
+    except Exception as e:
+
+        logging.exception("Problem!")
 
 except Exception as e:
     logging.exception('problem connecting toi database.')
